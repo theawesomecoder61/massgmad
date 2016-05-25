fb5bde845fda60dfa09e2ae43fd94200452dd0a9
Andrew <theac61@gmail.com>
2016-05-24 20:09:45 -0400

    Version 1.0

---
+++ MassGmad/NoGmod.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Windows.Forms
+
+Public Class NoGmod
+
+    Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK_Button.Click
+        Process.Start("steam://run/4000")
+    End Sub
+
+    Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel_Button.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
+    End Sub
+
+    Private Sub RichTextBox1_SelectionChanged(sender As Object, e As EventArgs) Handles RichTextBox1.SelectionChanged
+        RichTextBox1.SelectionLength = 0
+    End Sub
+End Class
